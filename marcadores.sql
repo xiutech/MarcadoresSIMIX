8718575cf460d65f78005be8255a971c9bbb2051
Ummyers <36709268+Ummyers@users.noreply.github.com>
2019-05-28 23:26:38 -0500

    Create marcadores.sql
    Para llenar la BD de SIMIX

---
+++ marcadores.sql
@@ -0,0 +1,20 @@
+INSERT INTO informador (correo,nombre,contrasenia) VALUES ('mu.andrea@ciencias.unam.mx','Andrea','12345');
+
+INSERT INTO tema (nombre, color, correo_informador) VALUES ('ENP', '#808fff',  'mu.andrea@ciencias.unam.mx');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.121773, 19.271162, 'ENP 1', 'Plantel Gabino Barreda ubicado en Xochimilco', 'ENP');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.099383, 19.383909, 'ENP 2', 'Plantel Erasmo Castellanos Quinto ubicado en Churubusco', 'ENP');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.095528, 19.482775, 'ENP 3', 'Plantel Justo Sierra ubicado en Constitución', 'ENP');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.195481, 19.403626, 'ENP 4', 'Plantel Vidal Castañeda y Najera ubicado en Observatorio', 'ENP');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.134713, 19.307540, 'ENP 5', 'Plantel José Vasconcelos ubicado en Coapa', 'ENP');
+
+INSERT INTO tema (nombre, color, correo_informador) VALUES ('Edificios 19s', '#80ffff',  'mu.andrea@ciencias.unam.mx');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.139801, 19.422760, 'Edificio Simón Bolívar','Esquina Chimilpopoca, edificio de 7 pisos.','Edificios 19s');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.171028, 19.412814, 'Edificio Ámsterdam','Esquina Laredo. Edificio habitacional de siete pisos. Cinco víctimas, cuatro heridos', 'Edificios 19s');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.122617, 19.303875, 'Girasoles I','Edificio habitacional de 5 pisos. Rescatadas 21 personas, ocho víctimas. Calzada del hueso y Rancho del arco', 'Edificios 19s');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.130508, 19.296956, 'Colegio Enrique Rébsamen', '26 Victimas, entre ellos niños', 'Edificios 19s');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.124328, 19.304094, 'Edificio comercial', 'Sin víctimas, Calzada del hueso y canal de miramontes','Edificios 19s');
+
+INSERT INTO tema (nombre, color, correo_informador) VALUES ('Canchas Futboleras', '#8fffff',  'mu.andrea@ciencias.unam.mx');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.120122, 19.306491, 'Planet Gol Coapa', 'Disponible para jugar futbol' ,'Edificios 19s');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.127814, 19.296411, 'Goal Fit Center', 'Calzada de las Brujas 98, ideal para jugar futbol' ,'Edificios 19s');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.127534, 19.285082, 'Glorieta de vaqueritos', 'Excelente cancha para futbol rapido.','Edificios 19s');
